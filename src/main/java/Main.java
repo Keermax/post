2ebe993c70e49e674e10640cd0e1ff176bc985bc
Your <you>
2023-02-27 14:21:25 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+        public static void main(String[] args) {
+            Post post = new Post();
+            post.name = "Иван";
+            post.passport = "4444 №444444";
+            post.patronymic = "Иванович";
+            post.phone = "7-977-123-12-21";
+            post.surname = "Иванов";
+            post.subscription = true;
+            post.birthday = new FormDate();
+
+            post.birthday.day = 13;
+            post.birthday.month = 06;
+            post.birthday.year = 1999;
+
+        }
+
+
+    }
+
+
